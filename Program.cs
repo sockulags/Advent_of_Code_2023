2e2a0104577e467b85462bfb2b887f4fdd1de726
Lucas Skog <lucasskog@gmail.com>
2024-01-05 20:13:00 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Advent_of_Code_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+
+       
+    }
+}
---
+++ Solver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2023
+{
+    public class Solver
+    {
+        public virtual void SolvePartOne()
+        {
+
+        }
+
+        public virtual void SolvePartTwo()
+        {
+
+        }
+    }
+}
