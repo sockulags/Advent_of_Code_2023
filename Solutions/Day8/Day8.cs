146d5b015eb36ee28d884f571a03a10573d33ed5
Lucas Skog <lucasskog@gmail.com>
2024-01-06 21:10:34 +0100

    Day 8 done

--- Solutions/Day8/Day8.cs
+++ Solutions/Day8/Day8.cs
@@ -1,10 +1,7 @@
 ﻿using SockulagsClassLibrary;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Advent_of_Code_2023.HelperClass;
+using System.Text.RegularExpressions;
 
 namespace Advent_of_Code_2023.Solutions
 {
@@ -22,15 +19,66 @@ namespace Advent_of_Code_2023.Solutions
 
             return Day.Answer(Date, p1, p2, sw.ElapsedMilliseconds);
         }
+        private long SolvePartOne(string[] input)
+        {
+            Dictionary<string, (string left, string right)> Nodes = new();
+            Regex rx = new Regex(@"(?i)[a-z]+");
+
+            for (int i = 2; i < input.Length; i++)
+            {
+                Nodes.Add(rx.Matches(input[i])[0].Value, (rx.Matches(input[i])[1].Value, rx.Matches(input[i])[2].Value));
+            }
+            return long.Parse(TotalSteps(Nodes, input[0]));
+        }
+        public static string TotalSteps(Dictionary<string, (string left, string right)> nodes, string path)
+        {
+            string current = "AAA";
+            int steps = 0;
+            int i = 0;
+            while (current != "ZZZ")
+            {
+                current = path[i] == 'L' ? nodes[current].left : nodes[current].right;
+                i = (i + 1) % path.Length;
+                steps++;
+            }
+            return steps.ToString();
+        }
 
         private long SolvePartTwo(string[] input)
         {
-            throw new NotImplementedException();
+            Dictionary<string, (string left, string right)> Nodes = new();
+            Regex rx = new Regex(@"(?i)[a-z]+");
+
+            for (int i = 2; i < input.Length; i++)
+            {
+                Nodes.Add(rx.Matches(input[i])[0].Value, (rx.Matches(input[i])[1].Value, rx.Matches(input[i])[2].Value));
+            }
+            var starts = Nodes.Keys.Where(x => x.EndsWith('A')).ToList();
+
+            List<long> totalSteps = new List<long>();
+            foreach (var start in starts)
+            {
+                totalSteps.Add(TotalStepsPartTwo(Nodes, input[0], start));
+            }
+
+            return totalSteps.LeastCommonMultiple();
         }
 
-        private long SolvePartOne(string[] input)
+        public static long TotalStepsPartTwo(Dictionary<string, (string left, string right)> nodes, string path, string current)
         {
-            throw new NotImplementedException();
+            long rotations = 0;
+            int i = 0;
+
+            while (!current.EndsWith('Z'))
+            {
+                current = path[i] == 'L' ? nodes[current].left : nodes[current].right;
+                i = (i + 1) % path.Length;
+                rotations++;
+            }
+            return rotations;
+
         }
+
+
     }
 }
---
+++ Utils/Utils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2023.HelperClass
+    {
+    public static class Utils
+    {
+        static long GreatestCommonFactor(long n1, long n2)
+        {
+            if (n2 == 0)
+            {
+                return n1;
+            }
+            else
+            {
+                return GreatestCommonFactor(n2, n1 % n2);
+            }
+        }
+
+        public static long LeastCommonMultiple(this List<long> numbers)
+        {
+            return numbers.Aggregate((S, val) => S * val / GreatestCommonFactor(S, val));
+        }
+    }
+}
