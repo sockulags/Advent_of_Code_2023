d85128ae66fa77530c58444cb249628e95eed5e2
Lucas Skog <lucasskog@gmail.com>
2024-01-07 18:20:29 +0100

    Added Lagrange formula to Maths class

--- Utils/Maths.cs
+++ Utils/Maths.cs
@@ -24,5 +24,14 @@ namespace Advent_of_Code_2023.HelperClass
         {
             return numbers.Aggregate((S, val) => S * val / GreatestCommonFactor(S, val));
         }
+
+        static long CalculateLagrange(long x, long[] values)
+        {
+            long term1 = ((x - 2) * (x - 4) / ((0 - 2) * (0 - 4))) * values[1];
+            long term2 = ((x - 0) * (x - 4) / ((2 - 0) * (2 - 4))) * values[2];
+            long term3 = ((x - 0) * (x - 2) / ((4 - 0) * (4 - 2))) * values[3];
+
+            return term1 + term2 + term3;
+        }
     }
 }
